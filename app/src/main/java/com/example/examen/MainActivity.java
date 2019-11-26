7c7d903c5a0d88330db24bb616346c09f843e9fb
Gilberto17 <gilberto.sanchez@pachuca.gob.mx>
2019-11-26 16:43:02 -0600

    Guardar y mostrar datos

--- app/src/main/java/com/example/examen/MainActivity.java
+++ app/src/main/java/com/example/examen/MainActivity.java
@@ -2,13 +2,23 @@ package com.example.examen;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private SharedPreferences infoDatos;
+    //VARIABLES GUARDADAS EN SHAREPREFERENCES
+    String mNombre, mEmail, mTelefono;
+
+    //VARIABLES GUARDADAS DE LOS CAMPOS DE TEXTO
+    EditText txtNombre, txtEmail, txtTelefono;
 
     Button mBtnDatos;
 
@@ -17,6 +27,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mBtnDatos = findViewById(R.id.btnDatos);
+
+        infoDatos = getSharedPreferences("misDatos",
+                Context.MODE_PRIVATE);
+        mNombre = infoDatos.getString("nombre", "");
+        mEmail = infoDatos.getString("email", "");
+        mTelefono = infoDatos.getString("telefono", "");
+
+        txtNombre = findViewById(R.id.nombre);
+        txtEmail = findViewById(R.id.email);
+        txtTelefono = findViewById(R.id.telefono);
+
+        txtNombre.setText(mNombre);
+        txtEmail.setText(mEmail);
+        txtTelefono.setText(mTelefono);
+
 
         mBtnDatos.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -28,6 +53,22 @@ public class MainActivity extends AppCompatActivity {
                         Toast.LENGTH_LONG).show();
             }
         });
+
+    }
+
+    public void guardarDatos(View view){
+        if(view.getId() == R.id.botonDatos){
+            SharedPreferences.Editor editor = infoDatos.edit();
+            editor.putString("nombre",txtNombre.getText().toString());
+            editor.putString("email",txtEmail.getText().toString());
+            editor.putString("telefono",txtTelefono.getText().toString());
+
+            editor.commit();
+            Toast.makeText(this,
+                    "Datos Guardados",
+                    Toast.LENGTH_LONG).show();
+
+        }
     }
 
     public void cambiarActivity(View view) {
--- app/src/main/java/com/example/examen/pantalla2.java
+++ app/src/main/java/com/example/examen/pantalla2.java
@@ -2,15 +2,25 @@ package com.example.examen;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class pantalla2 extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantalla2);
+
+
+
     }
 
 
 }
+
